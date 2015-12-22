aaf439fcd772c6f2ab13265c7e316ba3f13c3e0c
Seo Jong Cheol <Seo Jong Cheol@192.168.0.5>
2015-12-22 14:59:23 +0900

    1 jcseo

--- HelloWorld1/src/com/example/helloworld/MainActivity.java
+++ HelloWorld1/src/com/example/helloworld/MainActivity.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+// 수정한다
 public class MainActivity extends Activity {
 
 	@Override
